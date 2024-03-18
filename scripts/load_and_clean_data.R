175f623203f5b1a5fe3a8b50760ea1c09401a731
Jessica Sherr <jcrs@bu.edu>
2024-03-18 15:02:54 -0400

    script to load raw df

--- scripts/load_and_clean_data.R
+++ scripts/load_and_clean_data.R
@@ -1,13 +1,14 @@
 # This file is purely as an example. 
 # There are a few places
 
-library(tidyverse)
+suppressPackageStartupMessages(library(tidyverse))
 
-loan_data <- read_csv(here::here("dataset", "loan_refusal.csv"))
+
+mmr_data <- read_csv(here::here("dataset", "IHME_USA_MMR_STATE_RACE_ETHN_1999_2019_ESTIMATES_Y2023M07D03.CSV"))
 
 ## CLEAN the data
-loan_data_clean <- loan_data
-
-write_csv(loan_data_clean, file = here::here("dataset", "loan_refusal_clean.csv"))
-
-save(loan_data_clean, file = here::here("dataset/loan_refusal.RData"))
+# mmr_data_clean <- loan_data
+# 
+# write_csv(loan_data_clean, file = here::here("dataset", "loan_refusal_clean.csv"))
+# 
+# save(loan_data_clean, file = here::here("dataset/loan_refusal.RData"))
