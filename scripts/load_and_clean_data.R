9b06d203ee3d3dcbf05039537642bd894dd7b484
Jessica Sherr <jcrs@bu.edu>
2024-03-18 15:10:25 -0400

    cleaned out unnecessary columns

--- scripts/load_and_clean_data.R
+++ scripts/load_and_clean_data.R
@@ -7,7 +7,12 @@ suppressPackageStartupMessages(library(tidyverse))
 mmr_data <- read_csv(here::here("dataset", "IHME_USA_MMR_STATE_RACE_ETHN_1999_2019_ESTIMATES_Y2023M07D03.CSV"))
 
 ## CLEAN the data
-# mmr_data_clean <- loan_data
+
+# mmr_data_clean <- mmr_data
+
+mmr_data_clean <-
+mmr_data |> 
+  reframe(location_id, location_name, race_group, year_id, val, lower, upper)
 # 
 # write_csv(loan_data_clean, file = here::here("dataset", "loan_refusal_clean.csv"))
 # 
