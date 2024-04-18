a7b152aa28e6351e5bb1b00ebea11cb368a6e42d
Jessica Sherr <jcrs@bu.edu>
2024-04-18 10:24:17 -0400

    woooohooo! finally combined them!!!

--- scripts/load_healthcare_spending_data.R
+++ scripts/load_healthcare_spending_data.R
@@ -79,96 +79,125 @@ phc_percap_longer$year_id <- sub("Y", "", phc_percap_longer$Year)
 
 # create summaries for census regions
 region_means <- phc_percap_longer |>
-  group_by(region, year) |> 
+  group_by(region, year_id) |> 
   summarize(spending = mean(spending))
 
+colnames(region_means)[colnames(region_means) == "region"] <- "location_name"
+
+## make phc data combine-able
+colnames(phc_percap_longer)[colnames(phc_percap_longer) == "State_Name"] <- "location_name"
+
+phc_percap_longer$location_name[phc_percap_longer$location_name == "United States"] <- "National"
+
+phc_percap_longer <- phc_percap_longer |> 
+  reframe(location_name, year_id, spending)
+
+# stack region phc data and other phc data
+phc_combine <-
+  rbind(phc_percap_longer, region_means)
+
+phc_combine |> 
+  filter(location_name == "National")
+phc_combine |> 
+  filter(location_name == "United States")
+
+phc_combine <- phc_combine[phc_combine$location_name != "United States", ]
+
+
+## combine them
+combined_data <- merge(phc_combine, mmr_combine, by = c("location_name", "year_id"), all = TRUE)
+
+
+## save the data 
+write_csv(combined_data, file = here::here("dataset", "phc_mmr_combined.csv"))
+
+save(combined_data, file = here::here("dataset/phc_mmr.RData"))
 
 
 
 
 
-
-
-
-# medcaid
-medicaid_aggregate20 <- 
-  read_csv(here::here("dataset/healthcare_spending", "MEDICAID_AGGREGATE20.CSV"))
-
-medicaid_enrollment20 <- 
-  read_csv(here::here("dataset/healthcare_spending", "MEDICAID_ENROLLMENT20.CSV"))
-
-medicaid_per_enrollee20 <- 
-  read_csv(here::here("dataset/healthcare_spending", "MEDICAID_PER_ENROLLEE20.CSV"))
-
-colnames(medicaid_aggregate20)[5] <- "REGION_NAME"
-
-# medicaid_stacked <- 
-#   rbind(medicaid_aggregate20, medicaid_enrollment20, medicaid_per_enrollee20)
-
-# medicare
-medicare_aggregate20 <- 
-  read_csv(here::here("dataset/healthcare_spending", "MEDICARE_AGGREGATE20.CSV"))
-
-medicare_enrollment20 <- 
-  read_csv(here::here("dataset/healthcare_spending", "MEDICARE_ENROLLMENT20.CSV"))
-
-medicare_per_enrollee20 <- 
-  read_csv(here::here("dataset/healthcare_spending", "MEDICARE_PER_ENROLLEE20.CSV"))
-
-colnames(medicare_aggregate20)[5] <- "REGION_NAME"
-colnames(medicare_per_enrollee20)[5] <- "REGION_NAME"
-
-# medicare_stacked <- 
-#   rbind(medicare_aggregate20, medicare_enrollment20, medicare_per_enrollee20)
-
-# private health insurance
-phi_aggregate20 <- 
-  read_csv(here::here("dataset/healthcare_spending", "PHI_AGGREGATE20.CSV"))
-
-phi_enrollment20 <- 
-  read_csv(here::here("dataset/healthcare_spending", "PHI_ENROLLMENT20.CSV"))
-
-phi_per_enrollee20 <- 
-  read_csv(here::here("dataset/healthcare_spending", "PHI_PER_ENROLLEE20.CSV"))
-
-colnames(phi_aggregate20)[5] <- "REGION_NAME"
-colnames(phi_per_enrollee20)[5] <- "REGION_NAME"
-
-# phi_stacked <- 
-#   rbind(phi_aggregate20, phi_enrollment20, phi_per_enrollee20)
-
-# US OVERALL
-us_aggregate20 <- 
-  read_csv(here::here("dataset/healthcare_spending", "US_AGGREGATE20.CSV"))
-
-us_per_capita20 <- 
-  read_csv(here::here("dataset/healthcare_spending", "US_PER_CAPITA20.CSV"))
-
-us_population20 <- 
-  read_csv(here::here("dataset/healthcare_spending", "US_POPULATION20.CSV"))
-
-colnames(us_aggregate20)[5] <- "REGION_NAME"
-colnames(us_per_capita20)[5] <- "REGION_NAME"
-
-# us_stacked <- 
-#   rbind(us_aggregate20, us_per_capita20, us_population20)
-
-# stacked data frames
-
-# stacked <- rbind(medicaid_aggregate20, medicare_aggregate20)
 # 
-# healthcare_spending_enrollment_stacked 
-#   <- rbind()
-
-## CLEAN the data
-
-# mmr_data_clean <- mmr_data
 # 
-# mmr_data_clean <-
-#   mmr_data |> 
-#   reframe(location_id, location_name, race_group, year_id, val, lower, upper)
+# # medcaid
+# medicaid_aggregate20 <- 
+#   read_csv(here::here("dataset/healthcare_spending", "MEDICAID_AGGREGATE20.CSV"))
 # 
-# write_csv(mmr_data_clean, file = here::here("dataset", "mmr_clean.csv"))
+# medicaid_enrollment20 <- 
+#   read_csv(here::here("dataset/healthcare_spending", "MEDICAID_ENROLLMENT20.CSV"))
 # 
-# save(mmr_data_clean, file = here::here("dataset/mmr.RData"))
+# medicaid_per_enrollee20 <- 
+#   read_csv(here::here("dataset/healthcare_spending", "MEDICAID_PER_ENROLLEE20.CSV"))
 # 
+# colnames(medicaid_aggregate20)[5] <- "REGION_NAME"
+# 
+# # medicaid_stacked <- 
+# #   rbind(medicaid_aggregate20, medicaid_enrollment20, medicaid_per_enrollee20)
+# 
+# # medicare
+# medicare_aggregate20 <- 
+#   read_csv(here::here("dataset/healthcare_spending", "MEDICARE_AGGREGATE20.CSV"))
+# 
+# medicare_enrollment20 <- 
+#   read_csv(here::here("dataset/healthcare_spending", "MEDICARE_ENROLLMENT20.CSV"))
+# 
+# medicare_per_enrollee20 <- 
+#   read_csv(here::here("dataset/healthcare_spending", "MEDICARE_PER_ENROLLEE20.CSV"))
+# 
+# colnames(medicare_aggregate20)[5] <- "REGION_NAME"
+# colnames(medicare_per_enrollee20)[5] <- "REGION_NAME"
+# 
+# # medicare_stacked <- 
+# #   rbind(medicare_aggregate20, medicare_enrollment20, medicare_per_enrollee20)
+# 
+# # private health insurance
+# phi_aggregate20 <- 
+#   read_csv(here::here("dataset/healthcare_spending", "PHI_AGGREGATE20.CSV"))
+# 
+# phi_enrollment20 <- 
+#   read_csv(here::here("dataset/healthcare_spending", "PHI_ENROLLMENT20.CSV"))
+# 
+# phi_per_enrollee20 <- 
+#   read_csv(here::here("dataset/healthcare_spending", "PHI_PER_ENROLLEE20.CSV"))
+# 
+# colnames(phi_aggregate20)[5] <- "REGION_NAME"
+# colnames(phi_per_enrollee20)[5] <- "REGION_NAME"
+# 
+# # phi_stacked <- 
+# #   rbind(phi_aggregate20, phi_enrollment20, phi_per_enrollee20)
+# 
+# # US OVERALL
+# us_aggregate20 <- 
+#   read_csv(here::here("dataset/healthcare_spending", "US_AGGREGATE20.CSV"))
+# 
+# us_per_capita20 <- 
+#   read_csv(here::here("dataset/healthcare_spending", "US_PER_CAPITA20.CSV"))
+# 
+# us_population20 <- 
+#   read_csv(here::here("dataset/healthcare_spending", "US_POPULATION20.CSV"))
+# 
+# colnames(us_aggregate20)[5] <- "REGION_NAME"
+# colnames(us_per_capita20)[5] <- "REGION_NAME"
+# 
+# # us_stacked <- 
+# #   rbind(us_aggregate20, us_per_capita20, us_population20)
+# 
+# # stacked data frames
+# 
+# # stacked <- rbind(medicaid_aggregate20, medicare_aggregate20)
+# # 
+# # healthcare_spending_enrollment_stacked 
+# #   <- rbind()
+# 
+# ## CLEAN the data
+# 
+# # mmr_data_clean <- mmr_data
+# # 
+# # mmr_data_clean <-
+# #   mmr_data |> 
+# #   reframe(location_id, location_name, race_group, year_id, val, lower, upper)
+# # 
+# # write_csv(mmr_data_clean, file = here::here("dataset", "mmr_clean.csv"))
+# # 
+# # save(mmr_data_clean, file = here::here("dataset/mmr.RData"))
+# # 
